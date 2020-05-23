bcd340f6d830142fa074f5962a344bc34b1a9cd7
Wafelack <wafelack@protonmail.com>
2020-05-23 17:17:23 +0200

    Variable initialisation editing

--- SimplyPy_CLI.py
+++ SimplyPy_CLI.py
@@ -26,10 +26,13 @@ def saisie():
 
     if(splited[0] == 'let'):
         variables[splited[1]] = splited[2]
-        print('Val \'' + str(splited[2]) + '\' assigned to ' + str(splited[1]))
+        if(splited[2] != ''):
+            print('Val \'' + str(splited[2]) + '\' assigned to ' + str(splited[1]))
+        elif(splited[2] == ''):
+            print('Variable \'' + str(splited[1] + '\' initialized'))
 
     if(splited[0] == 'help'):
-        print("To display a string : println! { <text>\nTo display a variable : println! # <var>\nTo initialize a variable : let <var>\nTo modify a variable value : let <var> <value>\nTo concatenate two String variables : <str_a> + <str_b>")
+        print("To display a string : println! { <text>\nTo display a variable : println! # <var>\nTo initialize a variable : let <var> (don't forget the space)\nTo modify a variable value : let <var> <value>\nTo concatenate two String variables : <str_a> + <str_b>")
     
         
     elif(splited[0] != 'println!' and splited[0] != 'let' and splited[0] != 'help'):

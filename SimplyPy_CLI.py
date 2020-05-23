77a1b61310d2c0dc9550d12ce274ac2be5878294
Wafelack <wafelack@protonmail.com>
2020-05-23 18:30:58 +0200

    Adding of maths and modifying of string concatenation

--- SimplyPy_CLI.py
+++ SimplyPy_CLI.py
@@ -4,7 +4,7 @@ from math import sqrt as rc
 # This is the CLI version of SimplyPy
 # The instructions available are : 
 # println! { <string> and println! # <variable> DONE
-# Mathematicals operations :  '+' '-' '/' '*' 'sqrt' TODO
+# Mathematicals operations :  '+' '-' '/' '*' 'sqrt' DONE
 # Variables declaration : let <name> <value> DONE
 
 variables = dict()
@@ -35,17 +35,34 @@ def saisie():
     if(splited[0] == 'help'):
         print("To display a string : println! { <text>\nTo display a variable : println! # <var>\nTo initialize a variable : let <var> (don't forget the space)\nTo modify a variable value : let <var> <value>\nTo concatenate two String variables : <str_a> + <str_b>")
     
-
-
-    elif(splited[0] != 'println!' and splited[0] != 'let' and splited[0] != 'help' and splited[0] != 'sq['):
-        if(splited[0] in variables.keys() and splited[2] in variables.keys()):
-            if(splited[1] == '+'):
-                print(variables[splited[0]] + variables[splited[2]])
+    if(splited[0] == 'sqrt'):
         try:
-            arg1 = int(splited[0])
-            arg2 = int(splited[0])
+            nombre = int(splited[1])
+            print(rc(nombre))
         except:
-            print('Error, "' + splited[0] + '" doesn\'t exists')
+            print('You cannot calculate the square root of a string litteral !')
+    if(splited[0] == 'sc'):
+        print(variables[splited[1]] + variables[splited[2]])
+        
+
+    elif(splited[0] != 'println!' and splited[0] != 'let' and splited[0] != 'help' and splited[0] != 'sqrt' and splited[0] != 'sc'):      
+        if(int(splited[0]) > -255 and int(splited[0]) < 255 and int(splited[2]) > -255 and int(splited[2]) < 255):
+            arg1 = int(splited[0])
+            arg2 = int(splited[2])
+
+            if(splited[1] == '+'):
+                print(arg1 + arg2)
+            if(splited[1] == '-'):
+                print(arg1 - arg2)
+            if(splited[1] == '*'):
+                print(arg1 * arg2)
+            if(splited[1] == '/'):
+                print(arg1 / arg2)
+            if(splited[1] == '**'):
+                origin = arg1
+                for i in range(1, arg2):
+                    arg1 *= origin
+                print(arg1)
         else:
             print('Error, "' + splited[0] + '" doesn\'t exists')
 

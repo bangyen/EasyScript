b703e7c14b98d949ffedf8e9a677a47d6909548e
Wafelack <wafelack@protonmail.com>
2020-05-23 16:31:15 +0200

    Adding of variable declaration

--- SimplyPy_CLI.py
+++ SimplyPy_CLI.py
@@ -2,19 +2,32 @@
 
 # This is the CLI version of SimplyPy
 # The instructions available are : 
-# println! {<string> ; and println! #<variable> ;
+# println! {<string> and println! #<variable>
 # Mathematicals operations :  '+' '-' '/' '*' 'sqrt'
-# Variables declaration : let <name> <value> ;
+# Variables declaration : let <name> <value>
+
+variables = dict()
 
 def saisie():
     saisie = input('>>> $ ')
     splited = saisie.split(' ')
+
+    # Functionnal
 
     if(splited[0] == 'println!'):
         if(splited[1].startswith('{')):
             instruct = splited[1]
             toShow = instruct[1:]
             print(toShow)
+
+    # TO TEST
+    if(splited[0] == 'let'):
+        variables[splited[1]] = splited[2]
+        print('Val ' + str(splited[2] + ' assigned to ' + str(splited[1])))
+    
+        
     else:
-        print('Error,' + splited[0] + ' doesn\'t exists')
-saisie()
+        print('Error, "' + splited[0] + '" doesn\'t exists')
+
+while True:
+    saisie()

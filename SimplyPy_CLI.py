b634a37055702e89c16201fa18bca2026dcd41f8
Wafelack <wafelack@protonmail.com>
2020-05-23 16:20:58 +0200

    Non-debugged println! function for string commit

--- SimplyPy_CLI.py
+++ SimplyPy_CLI.py
@@ -2,7 +2,16 @@
 
 # This is the CLI version of SimplyPy
 # The instructions available are : 
-# println! "<string>" ; and println! <variable> ;
+# println! {<string> ; and println! #<variable> ;
 # Mathematicals operations :  '+' '-' '/' '*' 'sqrt'
 # Variables declaration : let <name> <value> ;
 
+def saisie():
+    saisie = input('>>> $ ')
+    splited = saisie.split(' ')
+
+    if(splited[0] == 'println!'):
+        if(splited[1].startswith('{')):
+            instruct = splited[1]
+            toShow = instruct[:-1]
+            print(toShow)

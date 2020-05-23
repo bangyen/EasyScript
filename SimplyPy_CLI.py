11f33912f9f6a4b36e97431d50e42c2135a5234e
Wafelack <wafelack@protonmail.com>
2020-05-23 16:14:33 +0200

    Adding of header

---
+++ SimplyPy_CLI.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+# This is the CLI version of SimplyPy
+# The instructions available are : 
+# println! "<string>" ; and println! <variable> ;
+# Mathematicals operations :  '+' '-' '/' '*' 'sqrt'
+# Variables declaration : let <name> <value> ;
+
